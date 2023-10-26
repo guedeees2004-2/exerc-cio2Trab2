fb2da309b43449a48c412a5ac7e2dbbcfa11503c
guedeees2004-2 <146488719+guedeees2004-2@users.noreply.github.com>
2023-10-26 18:08:56 +0000

    aleluia terminei

--- main.c
+++ main.c
@@ -1,43 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 char** criaSenhas (char** vetorStrings, int qtdElementos)
 {
     char** senhas = (char**) malloc(sizeof(char*) * qtdElementos);
     if (senhas == NULL)
     {
-        printf("Erro: Memoria insuficiente!\n");
+        printf("Errona alocacao de memoria!\n");
         return 0;
     }
 
+    // todos elementos são NULL de "Senhas"
+    for (int i = 0; i < qtdElementos; i++)
+    {
+        senhas[i] = NULL;
+    }
+
+    // copia os elementos não nulos de "vetorStrings" para "senhas"
     for (int i = 0; i < qtdElementos; i++)
     {
         if (vetorStrings[i] != NULL)
         {
             senhas[i] = (char*) malloc(sizeof(char) * (strlen(vetorStrings[i]) + 1));
-            if (senhas[i] == NULL)
-            {
-                printf("Erro: Memoria insuficiente!\n");
-                return 0;
-            }
             strcpy(senhas[i], vetorStrings[i]);
         }
     }
     return senhas;
 }
 
+char* valida(char** vetor, int tamanho, int usuario)
+{
+    int qtdNum = 0;
+    for (int i = 0; i < strlen(vetor[usuario]); i++)
+    {
+        if (isdigit(vetor[usuario][i]))
+            qtdNum++;
+    }
+    if (qtdNum == 0)
+        return ("Senha invalida");
+    else
+        return ("Senha valida");
+}
+
+void eliminaSenha(char** vetor, int usuario)
+{
+    free(vetor[usuario]);
+    vetor[usuario] = NULL;
+}
+
+void eliminaTodos(char** vetor, int tamanho)
+{
+    for (int i = 0; i < tamanho; i++)
+    {
+        eliminaSenha(vetor, i);
+    }
+}
+
+void printVetor(char** vetor, int qtdElementos)
+{
+    for (int i = 0; i < qtdElementos; i++)
+    {
+        if (vetor[i] == NULL)
+            printf("NULL, ");
+        else
+            printf("\"%s\", ", vetor[i]);
+    }
+}
+
 int main(void)
 {
     char* vetorSenhas[] =  {"an5a",NULL,"puc","9casa"};
-    int tamanho = sizeof(vetorSenhas) / sizeof(char*);
+    int tamanho = sizeof(vetorSenhas) / sizeof(vetorSenhas[0]);
     char** senhas = criaSenhas(vetorSenhas, tamanho);
     
     printf("Suas senhas sao:\n");
-    for (int i = 0; i < 4; i++)
-    {
-        printf("%s\n", senhas[i]);
-    }
+    printVetor(senhas, tamanho);
+    printf("\n");
+    printf("%s", valida(vetorSenhas, tamanho, 2));
 
+    printf("\n");
+    eliminaTodos(senhas, tamanho);
+    printVetor(senhas, tamanho);
     return 0;
 }
